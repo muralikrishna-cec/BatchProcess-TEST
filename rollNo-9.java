eb07ec0622534d05919af908e0f5a30d64308043
Murali Krishna <chn24mc018@ceconline.edu>
2025-08-31 22:45:18 +0530

    Create rollNo-9.java

---
+++ rollNo-9.java
@@ -0,0 +1,12 @@
+public class EarlyReturn {
+    public static boolean isEven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(4));
+    }
+}
