c05717a4bab4269605380cb97641a55fc4052c60
Murali Krishna <chn24mc018@ceconline.edu>
2025-08-31 22:43:45 +0530

    Create rollNo-6.java

---
+++ rollNo-6.java
@@ -0,0 +1,16 @@
+public class HelloTest {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        if (a + b > 7) {
+            System.out.println("Greater than 7");
+        } else {
+            System.out.println("7 or less");
+        }
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("i: " + i);
+        }
+    }
+}
