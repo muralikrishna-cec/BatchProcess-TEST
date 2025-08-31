40358e05a0fcaf0ff5424a377780eb15c2c9a51e
Murali Krishna <chn24mc018@ceconline.edu>
2025-08-31 22:38:38 +0530

    Create rollNo-1.java

---
+++ rollNo-1.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            total += i;
+        }
+        System.out.println("Total: " + total);
+    }
+}
