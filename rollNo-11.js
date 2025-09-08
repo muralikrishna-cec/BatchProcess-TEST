b82da5d06ecb3da58969a3cba8da6f510169cafc
Murali Krishna <chn24mc018@ceconline.edu>
2025-09-08 09:00:16 +0530

    Create rollNo-11.js

---
+++ rollNo-11.js
@@ -0,0 +1,11 @@
+let y = 15;
+
+if (y > 10) {
+    if (y < 20) {
+        console.log("y is between 10 and 20");
+    } else {
+        console.log("y is 20 or more");
+    }
+} else {
+    console.log("y is 10 or less");
+}
