1b4a1ca7001a6b831229856b6a00eae114060b4f
Murali Krishna <chn24mc018@ceconline.edu>
2025-09-21 08:12:51 +0530

    Create prime.py

---
+++ algorithms/prime.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    print("Prime 29:", is_prime(29))
+    print("Prime 15:", is_prime(15))
