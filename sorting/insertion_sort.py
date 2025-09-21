5f1663cbe9fcf70328a4bbc5051070fcada00bf9
Murali Krishna <chn24mc018@ceconline.edu>
2025-09-21 08:06:03 +0530

    Create insertion_sort.py

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+if __name__ == "__main__":
+    print("Insertion Sort:", insertion_sort([64, 25, 12, 22, 11]))
