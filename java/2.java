9d87700c04ea5d0ecd5e2cfcf131f3ea3f11b7f0
Murali Krishna <chn24mc018@ceconline.edu>
2025-09-21 08:17:54 +0530

    Create 2.java

---
+++ java/2.java
@@ -0,0 +1,13 @@
+
+public class EarlyReturn {
+    public static boolean isEven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(4));
+    }
+}
