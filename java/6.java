3187fa62064ed5c902185de0eb42b383d6b2e502
Murali Krishna <chn24mc018@ceconline.edu>
2025-09-21 08:21:06 +0530

    Create 6.java

---
+++ java/6.java
@@ -0,0 +1,27 @@
+public class CalculatorApp {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+  
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        if (x > y) {
+            printResults(x, y);
+        } else {
+            System.out.println("x is not greater than y");
+        }
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("i: " + i);
+        }
+    }
+}
