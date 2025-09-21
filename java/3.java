602301f811f92187fc941111da3959d968998964
Murali Krishna <chn24mc018@ceconline.edu>
2025-09-21 08:18:35 +0530

    Create 3.java

---
+++ java/3.java
@@ -0,0 +1,11 @@
+public class LoopTest {
+    public static void printLoop() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printLoop();
+    }
+}
